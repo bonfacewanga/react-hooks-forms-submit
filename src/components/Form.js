cea2368839addac4eb46b0914467e6821e19f811
Bonface <bonniewanga34@gmail.com>
2023-08-30 01:04:33 +0300

    completed assignments

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,7 +15,9 @@ function Form(props) {
   return (
     <form>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
+
       <input type="text" onChange={handleLastNameChange} value={lastName} />
+      
       <button type="submit">Submit</button>
     </form>
   );
